6d79e3d5b14803b1d1965ad948ecb9bc230c2bbf
ikrz <igor.krzywda@improvoc.com>
2022-06-02 22:17:24 +0200

    proof of concept with links on youtube

--- proof_of_concept.py
+++ proof_of_concept.py
@@ -30,8 +30,42 @@ def get_tool_timestamps(description):
     return tools
 
 
+def timestamp_in_seconds(timestamp: str):
+    h, m, s = [int(i) for i in timestamp.split(":")]
+    return h * 3600 + m * 60 + s
+
+
+def get_tool_data(description: str):
+    lines = description.splitlines()
+    timestamps = [l for l in lines if re.match(r"^\d{2}:\d{2}:\d{2}", l)]
+    tool_data = [
+        dict(
+            timestamp=timestamp_in_seconds(re.match(r"^\d{2}:\d{2}:\d{2}", t).group(0)),
+            description=re.sub(r"^\d{2}:\d{2}:\d{2}", "", t),
+        )
+        for t in timestamps
+        if "Tool" in t
+    ]
+    return tool_data
+
+
+def episode_rundown(video_data: dict):
+    description = video_data["snippet"]["description"]
+    tool_data = get_tool_data(description)
+    return [
+        dict(
+            **td,
+            title=video_data["snippet"]["title"],
+            link=f"https://www.youtube.com/watch?v={video_data['snippet']['resourceId']['videoId']}&t={td['timestamp']}",
+        )
+        for td in tool_data
+    ]
+
+
 if __name__ == "__main__":
-    description = get_description_data()[0]
-    tools = get_tool_timestamps(description)
-    for t in tools:
-        print(t)
+    videos = get_playlist_video_data()
+    video_data = next((i for i in videos["items"]), None)
+    rundown = episode_rundown(video_data)
+    print(rundown[0]["title"])
+    for r in rundown:
+        print(f"{r['description']} - {r['link']}")
