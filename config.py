dec41c0979b0ea48ef66e7452c786fdb13bf45ae
ikrz <igor.krzywda@improvoc.com>
2022-06-02 21:48:34 +0200

    getting timestamps with tools

--- config.py
+++ config.py
@@ -1,9 +1,13 @@
 from pydantic import BaseSettings
 
+
 class Settings(BaseSettings):
     YOUTUBE_API_KEY: str
+    YT_CHANNEL_ID: str
+    YT_PLAYLIST_ID: str
 
     class Config:
         env_file = ".env"
+
 
 settings = Settings()
--- proof_of_concept.py
+++ proof_of_concept.py
@@ -1,3 +1,37 @@
 from config import settings
 import requests
+import json
+import re
 
+
+def get_huberman_data():
+    response = requests.get(
+        f"https://www.googleapis.com/youtube/v3/playlists?part=snippet&channelId={settings.YT_CHANNEL_ID}&key={settings.YOUTUBE_API_KEY}"
+    )
+    print(json.dumps(response.json(), sort_keys=True, indent=4))
+
+
+def get_playlist_video_data():
+    request_url = f"https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&playlistId={settings.YT_PLAYLIST_ID}&maxResults=1000&key={settings.YOUTUBE_API_KEY}"
+    response = requests.get(request_url)
+    return response.json()
+
+
+def get_description_data():
+    videos = get_playlist_video_data()
+    descriptions = [i["snippet"]["description"] for i in videos["items"]]
+    return descriptions
+
+
+def get_tool_timestamps(description):
+    lines = description.splitlines()
+    timestamps = [l for l in lines if re.match(r"^\d{2}:\d{2}:\d{2}", l)]
+    tools = [t for t in timestamps if "Tool" in t]
+    return tools
+
+
+if __name__ == "__main__":
+    description = get_description_data()[0]
+    tools = get_tool_timestamps(description)
+    for t in tools:
+        print(t)
